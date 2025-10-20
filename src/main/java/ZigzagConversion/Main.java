a4faa006e383db1ec7b1d65793484d0cd187e800
Sharamet-K <Kirjasheremet@gmail.com>
2025-10-20 16:55:16 +0300

    zeg zaaag task solve

---
+++ src/main/java/ZigzagConversion/Main.java
@@ -0,0 +1,57 @@
+package ZigzagConversion;
+
+public class Main {
+
+
+    /**
+     *
+     * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+     *
+     * P   A   H   N
+     * A P L S I I G
+     * Y   I   R
+     * And then read line by line: "PAHNAPLSIIGYIR"
+     *
+     * Write the code that will take a string and make this conversion given a number of rows:
+     *
+     * string convert(string s, int numRows);
+     *
+     *
+     * Example 1:
+     *
+     * Input: s = "PAYPALISHIRING", numRows = 3
+     * Output: "PAHNAPLSIIGYIR"
+     * Example 2:
+     *
+     * Input: s = "PAYPALISHIRING", numRows = 4
+     * Output: "PINALSIGYAHRPI"
+     * Explanation:
+     * P     I    N
+     * A   L S  I G
+     * Y A   H R
+     * P     I
+     * Example 3:
+     *
+     * Input: s = "A", numRows = 1
+     * Output: "A"
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= s.length <= 1000
+     * s consists of English letters (lower-case and upper-case), ',' and '.'.
+     * 1 <= numRows <= 1000
+     */
+
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+
+        Solution solution = new Solution();
+        String result = solution.convert(s, numRows);
+
+        System.out.println("Result: " + result);
+    }
+
+
+}
